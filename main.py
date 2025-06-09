bb23231f24210fb4dff6c95442255f268ea125b4
alex <alexc.single@gmail.com>
2025-06-09 12:44:28 -0500

    modified:   main.py

--- main.py
+++ main.py
@@ -1,1 +1,51 @@
-print("hi")
+from gpiozero import Button
+from signal import pause
+import os
+from pathlib import Path
+import subprocess
+
+videos = {
+
+"vid1": "path/to/vid/",
+"vid2": "path/to/vid/",
+"vid3": "path/to/vid/",
+"vid4": "path/to/vid/",
+"vid5": "path/to/vid/",
+"vid6": "path/to/vid/",
+"vid7": "path/to/vid/",
+"vid8": "path/to/vid/",
+"vid9": "path/to/vid/",
+"vid10": "path/to/vid/",
+"vid11": "path/to/vid/",
+"vid12": "path/to/vid/",
+"vid13": "path/to/vid/",
+"vid14": "path/to/vid/",
+}
+
+pin_mapping = {
+
+
+
+}
+
+def play_video(n):
+    global player
+    file = Path(videos[n])
+    
+    #clean player
+    if player and player.poll() is None:
+        player.terminate()
+
+
+    player = subprocess.Popen([
+    "omxplayer",
+    "--no-osd",           # hide on-screen display
+    "--aspect-mode", "fill",  # full-screen scaling
+    #select the path to the video with input number being the n parameter passed
+    file
+]) 
+    
+
+
+
+   
