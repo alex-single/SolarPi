2514209de1d128d75773f9ce27e528fd70b46d09
alex <alexc.single@gmail.com>
2025-07-02 08:13:55 -0500

    modified:   DEBUG2.py

--- DEBUG2.py
+++ DEBUG2.py
@@ -1,10 +1,26 @@
 import RPi.GPIO as GPIO
 import time
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+# BCM pin numbers for all usable GPIO pins on Pi Zero (excluding power/GND/UART/I2C/SPI-only pins)
+pins_to_test = [4, 5, 6, 12, 13, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27]
 
-while True:
-    if GPIO.input(17) == GPIO.LOW:
-        print("Button Pressed")
-    time.sleep(0.1)
+GPIO.setmode(GPIO.BCM)
+
+# Setup all pins as inputs with internal pull-up resistors
+for pin in pins_to_test:
+    GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+print("Press any connected button to see which GPIO pin was triggered (CTRL+C to stop)")
+
+try:
+    while True:
+        for pin in pins_to_test:
+            if GPIO.input(pin) == GPIO.LOW:
+                print(f"Button pressed on GPIO pin {pin}")
+        time.sleep(0.1)
+
+except KeyboardInterrupt:
+    print("\nExiting...")
+
+finally:
+    GPIO.cleanup()
